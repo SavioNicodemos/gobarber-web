f672a557cfbfc47db7f79ff68acc8e08fae094bd
Savio Nicodemos <saviogomes_182@hotmail.com>
2021-01-14 21:36:01 -0300

    finalizado telas de logine cadastro com validações

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => {
-  return <h1>Hello World!</h1> ;
-}
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
